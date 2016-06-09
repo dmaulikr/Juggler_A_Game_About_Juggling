b17dfab4997d3b995f7eec41af5b59407425d66a
Evan Lewis <evanlws12@gmail.com>
2016-06-09 17:43:22 -0400

    Implemented Chartboost

--- Juggler/AppDelegate.swift
+++ Juggler/AppDelegate.swift
@@ -16,9 +16,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     Fabric.with([Crashlytics.self, GameAnalytics.self])
+    
+    // Enable log to output simple details (disable in production)
+    GameAnalytics.setEnabledInfoLog(true)
+    // Enable log to output full event JSON (disable in production)
+    GameAnalytics.setEnabledVerboseLog(true)
+    
+    // Example: configure available virtual currencies and item types for later use in resource events
+    // GameAnalytics.configureAv$ailableResourceCurrencies(["gems", "gold"])
+    // GameAnalytics.configureAvailableResourceItemTypes(["boost", "lives"])
+    
+    // Example: configure available custom dimensions for later use when specifying these
+    // GameAnalytics.configureAvailableCustomDimensions01(["ninja", "samurai"])
+    // GameAnalytics.configureAvailableCustomDimensions02(["whale", "dolphin"])
+    // GameAnalytics.configureAvailableCustomDimensions03(["horde", "alliance"])
+    
+    // Configure build version
+    GameAnalytics.configureBuild("1.0.0")
+    
+    // initialize GameAnalytics - this method will use app keys injected by Fabric
+    GameAnalytics.initializeWithConfiguredGameKeyAndGameSecret()
+    // to manually specify keys use this method:
+    //GameAnalytics.initializeWithGameKey("[game_key]", gameSecret:"[game_secret]")
+    
+    // Initialize the Chartboost library
+    Chartboost.startWithAppId("538794871873da029599b73f", appSignature: "673ce4e7a34816fe6867a232ec58c1f7452900cb", delegate: nil)
+    Chartboost.setShouldRequestInterstitialsInFirstSession(false)
+    
     return true
   }
 
--- Juggler/GameViewController.swift
+++ Juggler/GameViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SpriteKit
+import GameAnalytics
 
 class GameViewController: UIViewController {
 
